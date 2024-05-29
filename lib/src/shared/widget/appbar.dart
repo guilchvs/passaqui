ae2cbd5190c329337d52c5aeaf9debd1a64b5630
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-05-28 22:06:42 -0300

    feat: add logo to appbar

--- lib/src/shared/widget/appbar.dart
+++ lib/src/shared/widget/appbar.dart
@@ -6,8 +6,19 @@ class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      elevation: 0,
-      backgroundColor: Theme.of(context).primaryColor,
+        elevation: 0,
+        backgroundColor: Theme.of(context).primaryColor,
+        leading: Container(decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(40),
+    ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Image.asset(
+            "assets/images/passaqui_nav_logo.jpeg",
+            height: 20,
+          )
+        ),
+      ),
       title: const Text(r"Pa$$ aqui"),
     );
   }
@@ -15,3 +26,4 @@ class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
   @override
   Size get preferredSize => const Size.fromHeight(56.0);
 }
+
