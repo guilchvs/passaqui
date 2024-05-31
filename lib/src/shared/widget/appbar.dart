1329e39e995f1819f6a0dfde6f75579c25ba7d62
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-05-31 17:05:28 -0300

    fix: fix appbar to render logo

--- lib/src/shared/widget/appbar.dart
+++ lib/src/shared/widget/appbar.dart
@@ -1,42 +1,77 @@
+//import 'package:flutter/material.dart';
+//import 'package:flutter_svg/flutter_svg.dart';
+//
+//class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
+//  final bool showBackButton;
+//  final bool showLogo;
+//
+//  const PassaquiAppBar(
+//      {this.showBackButton = false, this.showLogo = true, Key? key})
+//      : super(key: key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return AppBar(
+//      key: key,
+//      elevation: 0,
+//      backgroundColor: Theme.of(context).primaryColor,
+//      leading: showBackButton
+//          ? IconButton(
+//              icon: const Icon(Icons.arrow_back),
+//              onPressed: () {
+//                Navigator.of(context).pop();
+//              },
+//            )
+//          : null,
+//      title: showLogo
+//          ? SvgPicture.asset(
+//              "assets/images/logo_passaqui_hor_1.svg",
+//              fit: BoxFit.contain,
+//              height: 100,
+//              width: 20,
+//            )
+//          : null,
+//    );
+//  }
+//
+//  @override
+//  Size get preferredSize => const Size.fromHeight(56.0);
+//}
+
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool showBackButton;
   final bool showLogo;
 
-  const PassaquiAppBar({this.showBackButton = false, this.showLogo = true, Key? key}) : super(key: key);
+  const PassaquiAppBar({
+    this.showBackButton = false,
+    this.showLogo = true,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AppBar(
-      key: key, // Pass the key to the AppBar constructor
+      key: key,
       elevation: 0,
       backgroundColor: Theme.of(context).primaryColor,
-      leading: showBackButton
-          ? IconButton(
-              icon: Icon(Icons.arrow_back),
-              onPressed: () {
-                Navigator.of(context).pop();
-              },
+      automaticallyImplyLeading: showBackButton,
+      title: showLogo
+          ? Padding(
+              padding: const EdgeInsets.only(left: 1.0),
+              child: Image.asset(
+                "assets/images/logo_passaqui_hor_1.png",
+                fit: BoxFit.contain,
+                height: 100,
+                width: 90,
+              ),
             )
-          : showLogo
-              ? Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(40),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(10),
-                    child: Image.asset(
-                      "assets/images/passaqui_nav_logo.jpeg",
-                      height: 20,
-                    ),
-                  ),
-                )
-              : null,
+          : null,
     );
   }
 
   @override
   Size get preferredSize => const Size.fromHeight(56.0);
 }
-
