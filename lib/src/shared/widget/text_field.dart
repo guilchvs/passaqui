2f42373734ec553871dec07b5cc30411e8ba4b70
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-05 20:26:06 -0300

    feat: redesign text field

--- lib/src/shared/widget/text_field.dart
+++ lib/src/shared/widget/text_field.dart
@@ -1,24 +1,24 @@
-
 import 'package:flutter/material.dart';
 
 class PassaquiTextField extends StatelessWidget {
-
-  final String label;
+  final String? label;
+  final String placeholder;
   final bool showVisibility;
   final bool isVisible;
-  final Function? toggleVisibility;
+  final VoidCallback? toggleVisibility;
   final TextEditingController editingController;
   final Function(String)? onChange;
 
   const PassaquiTextField({
-    required this.label,
+    this.label,
     required this.editingController,
+    this.placeholder = "",
     this.showVisibility = false,
     this.isVisible = true,
     this.toggleVisibility,
     this.onChange,
-    super.key
-  });
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -27,35 +27,65 @@ class PassaquiTextField extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text(
-            label,
-            style: const TextStyle(
+          if (label != null)
+            Text(
+              label!,
+              style: const TextStyle(
                 fontSize: 16,
                 color: Color(0xFF484C4F),
                 fontFamily: 'Raleway',
-                fontWeight: FontWeight.w700),
-          ),
-          const SizedBox(height: 8,),
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          const SizedBox(height: 8),
           TextField(
             controller: editingController,
             onChanged: onChange,
             obscureText: !isVisible,
-            decoration: InputDecoration(
-              suffixIcon: !showVisibility ? const SizedBox() : GestureDetector(
-                child: Icon(isVisible ? Icons.visibility_outlined : Icons.visibility_off_outlined),
-              ),
-
-              border: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(50.0),
-              ),
-              filled: true,
-              hintStyle: TextStyle(color: Colors.grey[800]),
-              hintText: "",
-              fillColor: Colors.white70,
+            style: const TextStyle(
+              fontSize: 16,
+              color: Colors.white,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w400,
             ),
-          )
+            decoration: InputDecoration(
+              suffixIcon: !showVisibility
+                  ? const SizedBox()
+                  : GestureDetector(
+                      onTap: toggleVisibility,
+                      child: Icon(
+                        isVisible
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        color: Colors.white,
+                      ),
+                    ),
+              enabledBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Color(0xFFA8CA4B),
+                  width: 2.0,
+                ),
+              ),
+              focusedBorder: const UnderlineInputBorder(
+                borderSide: BorderSide(
+                  color: Color(0xFFA8CA4B),
+                  width: 2.0,
+                ),
+              ),
+              filled: false,
+              hintStyle: const TextStyle(
+                fontFamily: 'Inter',
+                fontSize: 16,
+                fontWeight: FontWeight.w400,
+                height: 60.51 / 50,
+                color: Colors.white,
+              ),
+              hintText: placeholder,
+            ),
+          ),
         ],
       ),
     );
   }
 }
+
