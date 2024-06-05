9456766268ed751cc0b650f9847b1f8824464b72
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-05 19:57:57 -0300

    feat: align text to the left, add icon, change background color

--- lib/src/modules/auth/login/login_screen.dart
+++ lib/src/modules/auth/login/login_screen.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:passaqui/src/core/app_theme.dart';
 import 'package:passaqui/src/core/di/service_locator.dart';
 import 'package:passaqui/src/core/navigation/navigation_handler.dart';
 import 'package:passaqui/src/modules/auth/create-account/create_account_screen.dart';
 import 'package:passaqui/src/modules/auth/forgot_password/forgot_password_screen.dart';
 import 'package:passaqui/src/modules/auth/login/login_controller.dart';
 import 'package:passaqui/src/modules/withdraw/welcome/withdraw_welcome_screen.dart';
-import 'package:passaqui/src/shared/widget/appbar.dart';
 import 'package:passaqui/src/shared/widget/button.dart';
 import 'package:passaqui/src/shared/widget/checkbox.dart';
 import 'package:passaqui/src/shared/widget/link.dart';
@@ -21,7 +21,6 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
-
   late LoginController controller;
 
   late TextEditingController userInputController;
@@ -38,98 +37,122 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PassaquiAppBar(showBackButton: false, showLogo: true),
+      backgroundColor: AppTheme.primaryColor,
       body: Column(
         children: [
           const SizedBox(
             height: 24,
           ),
           Expanded(
-            child: Container(
-              width: double.infinity,
-              decoration: const BoxDecoration(
-                borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(26),
-                    topRight: Radius.circular(26)),
-                color: Color(0xFFF0F0F0),
-              ),
-              child: Column(
-                children: [
-                  const SizedBox(height: 64,),
-                  const Text(
-                    "Acessar minha conta",
-                    style: TextStyle(
-                        fontSize: 22,
-                        color: Color(0xFF136048),
-                        fontFamily: 'Raleway',
-                        fontWeight: FontWeight.w700),
-                  ),
-                  const SizedBox(height: 56,),
-                  PassaquiTextField(
-                    editingController: userInputController,
-                    label: "Usuário",
-                  ),
-                  const SizedBox(height: 16,),
-                  PassaquiTextField(
-                    editingController: passwordInputController,
-                    label: "Senha",
-                    isVisible: true,
-                    showVisibility: true,
-                  ),
-                  const SizedBox(height: 16,),
-
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 16,
+                ),
+                Image.asset(
+                  'assets/images/logo_passaqui_hor_1.png',
+                  fit: BoxFit.cover,
+                  height: 120,
+                  width: 160,
+                ),
+                const SizedBox(
+                  height: 64,
+                ),
+                const Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 24.0),
+                      child: Text(
+                        "Acesse sua conta:",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                            fontSize: 25,
+                            color: Colors.white,
+                            fontFamily: 'Inter',
+                            fontWeight: FontWeight.w600),
+                      ),
                     ),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        PassaquiCheckBox(
-                          label: "Lembrar senha",
-                          onChanged: controller.setRememberPassword,
-                          value: controller.rememberPassword,
-                        ),
-                        PassaquiLink(
-                          label: "Esqueci a senha",
-                          onTap: (){
-                            DIService().inject<NavigationHandler>().navigate(ForgotPasswordScreen.route);
-                          },
-                        ),
-                      ],
-                    ),
+                  ],
+                ),
+                const SizedBox(
+                  height: 56,
+                ),
+                PassaquiTextField(
+                  editingController: userInputController,
+                  label: "Usuário",
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                PassaquiTextField(
+                  editingController: passwordInputController,
+                  label: "Senha",
+                  isVisible: true,
+                  showVisibility: true,
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      PassaquiCheckBox(
+                        label: "Lembrar senha",
+                        onChanged: controller.setRememberPassword,
+                        value: controller.rememberPassword,
+                      ),
+                      PassaquiLink(
+                        label: "Esqueci a senha",
+                        onTap: () {
+                          DIService()
+                              .inject<NavigationHandler>()
+                              .navigate(ForgotPasswordScreen.route);
+                        },
+                      ),
+                    ],
                   ),
-                  const SizedBox(height: 40,),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(
-                      horizontal: 24
-                    ),
-                    child: PassaquiButton(
-                      label: "Continuar",
-                      minimumSize: const Size(200, 40),
-                      onTap: (){
-                        DIService().inject<NavigationHandler>().navigate(WithdrawWelcomeScreen.route);
-                      },
-                    ),
-                  ),
-                  const SizedBox(height: 16,),
-                  Container(
-                    margin: const EdgeInsets.symmetric(
-                      horizontal: 24.0,
-                    ),
-                    width: double.infinity,
-                    height: 1.4,
-                    color: const Color(0xFF4D5D71),
-                  ),
-                  const SizedBox(height: 16,),
-                  PassaquiLink(
-                    label: "Criar uma conta",
-                    onTap: (){
-                      DIService().inject<NavigationHandler>().navigate(CreateAccountScreen.route);
+                ),
+                const SizedBox(
+                  height: 40,
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 24),
+                  child: PassaquiButton(
+                    label: "Continuar",
+                    minimumSize: const Size(200, 40),
+                    onTap: () {
+                      DIService()
+                          .inject<NavigationHandler>()
+                          .navigate(WithdrawWelcomeScreen.route);
                     },
                   ),
-                ],
-              ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                Container(
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 24.0,
+                  ),
+                  width: double.infinity,
+                  height: 1.4,
+                  color: const Color(0xFF4D5D71),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                PassaquiLink(
+                  label: "Criar uma conta",
+                  onTap: () {
+                    DIService()
+                        .inject<NavigationHandler>()
+                        .navigate(CreateAccountScreen.route);
+                  },
+                ),
+              ],
             ),
           )
         ],
