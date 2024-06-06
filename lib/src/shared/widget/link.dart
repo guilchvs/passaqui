aa110f521211bc3c5c12c838f04b8ec513afc4a3
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-05 21:04:57 -0300

    feat: redesign passaqui link

--- lib/src/shared/widget/link.dart
+++ lib/src/shared/widget/link.dart
@@ -1,29 +1,75 @@
+//import 'package:flutter/material.dart';
+//
+//class PassaquiLink extends StatelessWidget {
+//  final String label;
+//  final Function onTap;
+//  final bool underline;
+//
+//  const PassaquiLink({
+//    required this.label,
+//    required this.onTap,
+//    this.underline = false,
+//
+//    Key? key,
+//  }) : super(key: key);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return GestureDetector(
+//      onTap: () {
+//        onTap.call();
+//      },
+//      child: Text(
+//        label,
+//        style: TextStyle(
+//          color: Colors.white,
+//          fontSize: 13,
+//          decoration: underline ? TextDecoration.underline : TextDecoration.none,
+//        ),
+//      ),
+//    );
+//  }
+//}
+//
+
 import 'package:flutter/material.dart';
 
 class PassaquiLink extends StatelessWidget {
-
   final String label;
   final Function onTap;
-
+  final bool underline;
 
   const PassaquiLink({
     required this.label,
     required this.onTap,
-    super.key});
+    this.underline = false,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: (){
-        onTap.call();
-      },
-      child: Text(label,
-        style: const TextStyle(
-            color: Color(0xFF189973),
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
-            decoration: TextDecoration.underline
-        ),
+      onTap: onTap as void Function()?,
+      child: Stack(
+        children: [
+          Text(
+            label,
+            style: const TextStyle(
+              color: Colors.white,
+              fontSize: 13,
+            ),
+          ),
+          if (underline)
+            Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: 1,
+                color: Colors.white,
+              ),
+            ),
+        ],
       ),
     );
   }
