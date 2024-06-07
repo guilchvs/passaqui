800e16ea494b50536d4721e0f623f6e8898ce5f0
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-06 22:10:02 -0300

    feat: add styles to button

--- lib/src/shared/widget/button.dart
+++ lib/src/shared/widget/button.dart
@@ -1,85 +1,58 @@
-// import 'package:flutter/material.dart';
-// 
-// class PassaquiButton extends StatelessWidget {
-// 
-//   final String label;
-//   final Function()? onTap;
-//   final Size? minimumSize;
-//   final bool isLight;
-// 
-//   const PassaquiButton({
-//     required this.label,
-//     this.onTap,
-//     this.minimumSize,
-//     this.isLight = true,
-//     super.key
-//   });
-// 
-//   @override
-//   Widget build(BuildContext context) {
-//     return SizedBox(
-//       width: double.infinity,
-//       child: ElevatedButton(
-//           style: ButtonStyle(
-//             minimumSize: minimumSize != null ? WidgetStatePropertyAll(minimumSize) : null,
-//             shape: WidgetStateProperty.all<RoundedRectangleBorder>(
-//                 RoundedRectangleBorder(
-//                   borderRadius: BorderRadius.circular(18.0),
-//                 )
-//             ),
-//             backgroundColor: WidgetStateProperty.resolveWith<Color>(
-//                   (Set<WidgetState> states) {
-//                 if (states.contains(WidgetState.disabled)) {
-//                   return isLight ? const Color(0xFF136048).withOpacity(.25) : const Color(0xFFA8CA4B).withOpacity(.25);
-//                 }
-//                 else{
-//                   return isLight ? const Color(0xFF136048) : const Color(0xFFA8CA4B);
-//                 }
-//               },
-//             ),
-// 
-//           ),
-//           onPressed: onTap,
-//           child: Text(
-//             label,
-//             style: const TextStyle(
-//                 fontFamily: 'Raleway',
-//                 fontWeight: FontWeight.w700,
-//                 fontSize: 16,
-//                 color: Colors.white
-//             ),
-//           )
-//       ),
-//     );
-//   }
-// }
-
 import 'package:flutter/material.dart';
+
+enum PassaquiButtonStyle { primary, secondary, defaultLight, defaultDark }
 
 class PassaquiButton extends StatelessWidget {
   final String label;
   final Function()? onTap;
   final Size? minimumSize;
-  final bool isLight;
+  final PassaquiButtonStyle style;
   final bool showArrow;
+  final bool centerText;
+  final double width;
+  final double height;
 
   const PassaquiButton({
     required this.label,
     this.onTap,
     this.minimumSize,
-    this.isLight = true,
+    this.style = PassaquiButtonStyle.defaultLight,
     this.showArrow = false,
+    this.centerText = false,
+    this.width = 350,
+    this.height = 60,
     Key? key,
   }) : super(key: key);
 
-  @override
-  Widget build(BuildContext context) {
-    return SizedBox(
-      height: 60, 
-      width: 350,
-      child: ElevatedButton(
-        style: ButtonStyle(
-          minimumSize: minimumSize != null ? MaterialStateProperty.all(minimumSize!) : null,
+  ButtonStyle _getButtonStyle() {
+    switch (style) {
+      case PassaquiButtonStyle.primary:
+        return ButtonStyle(
+          backgroundColor:
+              MaterialStateProperty.all<Color>(const Color.fromRGBO(18, 96, 73, 1)),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white), // Changed to white
+          minimumSize: MaterialStateProperty.all<Size>(minimumSize ?? const Size(0, 0)),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+            ),
+          ),
+        );
+      case PassaquiButtonStyle.secondary:
+        return ButtonStyle(
+          backgroundColor: MaterialStateProperty.all<Color>(Colors.transparent),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          overlayColor: MaterialStateProperty.all<Color>(Colors.white.withOpacity(0.1)),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(10.0),
+              side: const BorderSide(color: Color.fromRGBO(168, 202, 75, 1), width: 2),
+            ),
+          ),
+        );
+      case PassaquiButtonStyle.defaultDark:
+        return ButtonStyle(
+          minimumSize: MaterialStateProperty.all<Size>(minimumSize ?? const Size(0, 0)),
           shape: MaterialStateProperty.all<RoundedRectangleBorder>(
             RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(18.0),
@@ -88,31 +61,90 @@ class PassaquiButton extends StatelessWidget {
           backgroundColor: MaterialStateProperty.resolveWith<Color>(
             (Set<MaterialState> states) {
               if (states.contains(MaterialState.disabled)) {
-                return isLight ? const Color(0xFF136048).withOpacity(0.25) : const Color(0xFFA8CA4B).withOpacity(0.25);
+                return const Color(0xFFA8CA4B).withOpacity(0.25);
               } else {
-                return const Color(0xFFA8CA4B); 
+                return const Color(0xFFA8CA4B);
               }
             },
           ),
-        ),
+        );
+      case PassaquiButtonStyle.defaultLight:
+      default:
+        return ButtonStyle(
+          minimumSize: MaterialStateProperty.all<Size>(minimumSize ?? const Size(0, 0)),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(18.0),
+            ),
+          ),
+          backgroundColor: MaterialStateProperty.resolveWith<Color>(
+            (Set<MaterialState> states) {
+              if (states.contains(MaterialState.disabled)) {
+                return const Color(0xFF136048).withOpacity(0.25);
+              } else {
+                return const Color(0xFFA8CA4B);
+              }
+            },
+          ),
+        );
+    }
+  }
+
+  TextStyle _getTextStyle() {
+    switch (style) {
+      case PassaquiButtonStyle.primary:
+      case PassaquiButtonStyle.secondary:
+        return const TextStyle(
+          fontFamily: 'Inter',
+          fontWeight: FontWeight.w500,
+          fontSize: 18,
+          color: Colors.white, 
+        );
+      default:
+        return const TextStyle(
+          fontFamily: 'Inter',
+          fontWeight: FontWeight.w500,
+          fontSize: 18,
+          color: Color(0xFF202020),
+        );
+    }
+  }
+
+  Icon _getIconStyle() {
+    switch (style) {
+      case PassaquiButtonStyle.primary:
+      case PassaquiButtonStyle.secondary:
+        return const Icon(
+          Icons.arrow_forward,
+          color: Colors.white, 
+        );
+      default:
+        return const Icon(
+          Icons.arrow_forward,
+          color: Color(0xFF202020),
+        );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: height,
+      width: width,
+      child: ElevatedButton(
+        style: _getButtonStyle(),
         onPressed: onTap,
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment: centerText
+              ? MainAxisAlignment.center
+              : MainAxisAlignment.spaceBetween,
           children: [
             Text(
               label,
-              style: const TextStyle(
-                fontFamily: 'Inter',
-                fontWeight: FontWeight.w500,
-                fontSize: 18,
-                color: const Color(0xFF202020),
-              ),
+              style: _getTextStyle(),
             ),
             if (showArrow)
-              Icon(
-                Icons.arrow_forward,
-                color: const Color(0xFF202020),
-              ),
+              _getIconStyle(),
           ],
         ),
       ),
