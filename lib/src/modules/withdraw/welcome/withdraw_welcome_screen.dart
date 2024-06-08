e4a7ef3ca1d42a9b62e1e80db2309d7c03fba770
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-07 22:00:39 -0300

    feat: update withdraw screen

--- lib/src/modules/withdraw/welcome/withdraw_welcome_screen.dart
+++ lib/src/modules/withdraw/welcome/withdraw_welcome_screen.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 import 'package:passaqui/src/core/di/service_locator.dart';
 import 'package:passaqui/src/core/navigation/navigation_handler.dart';
 import 'package:passaqui/src/modules/withdraw/steps/withdraw_step_screen.dart';
-import 'package:passaqui/src/shared/widget/appbar.dart';
 import 'package:passaqui/src/shared/widget/button.dart';
 
 class WithdrawWelcomeScreen extends StatefulWidget {
@@ -19,61 +17,97 @@ class _WithdrawWelcomeScreenState extends State<WithdrawWelcomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: const PassaquiAppBar(),
       body: SafeArea(
         child: Center(
           child: Padding(
-            padding: const EdgeInsets.symmetric(
-              horizontal: 32
-            ),
+            padding: const EdgeInsets.symmetric(horizontal: 32),
             child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.start, // Align text to the left
               children: [
                 const SizedBox(
                   height: 40,
                 ),
-                const Text(
-                  "Como realizar a contratação?",
-                  style: TextStyle(
-                      fontSize: 22,
-                      color: Color(0xFF1E1E1E),
-                      fontFamily: 'Raleway',
-                      fontWeight: FontWeight.w700),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height /
+                          3, // Set height to 1/3 of screen height
+                      width: MediaQuery.of(context)
+                          .size
+                          .width, // Set width to full screen width
+                      child: Image.asset(
+                        "assets/images/homem.png",
+                        fit: BoxFit.cover, // Adjust the fit as needed
+                      ),
+                    ),
+                    RichText(
+                      text: const TextSpan(
+                        children: [
+                          TextSpan(
+                            text: "Como realizar\n",
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Color.fromRGBO(18, 96, 73, 1),
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight
+                                  .w700, // Weight 700 for "Como realizar"
+                              height: 1.5, // Line height for "Como realizar"
+                            ),
+                          ),
+                          TextSpan(
+                            text: "a contratação?",
+                            style: TextStyle(
+                              fontSize: 22,
+                              color: Color.fromRGBO(18, 96, 73, 1),
+                              fontFamily: 'Inter',
+                              fontWeight: FontWeight
+                                  .w400, // Weight 400 for "a contratação"
+                              height: 1.5, // Line height for "a contratação"
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
                 ),
                 const SizedBox(
-                  height: 32,
-                ),
-                SvgPicture.asset('assets/images/passaqui_logo.svg', height: 200),
-                const SizedBox(
-                  height: 40,
+                  height: 25,
                 ),
                 const Padding(
                   padding: EdgeInsets.only(
-                    right: 20, 
-                    left: 20, 
+                    right: 20,
                   ),
                   child: Text(
-                    "Para simular e antecipar seu saque aniversário, você precisa ter habilitado a modalidade e autorizado nosso banco a consultar seu saldo no aplicativo do FGTS.",
+                    "Para simular e antecipar seu Saque-Aniversário você precisa ter habilitado a modalidade e autorizado nossa instituição a consultar o saldo pelo aplicativo do FGTS.",
                     style: TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.w500,
-                      fontFamily: 'Raleway',
-                      color: Color(0xFF1E1E1E)
-                    ),
-                    textAlign: TextAlign.center,
+                        fontSize: 16,
+                        fontWeight: FontWeight.w400,
+                        fontFamily: 'Inter',
+                        color: Color(0xFF1E1E1E)),
+                    textAlign: TextAlign.left, // Text alignment left
                   ),
                 ),
-                const SizedBox(height: 120),
+                const SizedBox(height: 40),
                 const PassaquiButton(
-                  label: "Veja como autorizar"
+                  label: "Habilitar Saque-Aniversário",
+                  textColor: Color.fromRGBO(18, 96, 73, 1),
+                  borderColor: Color.fromRGBO(18, 96, 73, 1),
+                  style: PassaquiButtonStyle.secondary,
                 ),
-                const Text("Ou"),
+                const SizedBox(height: 20),
                 PassaquiButton(
-                    label: "Habilitar Saque-Aniversário",
-                  onTap: (){
-                    DIService().inject<NavigationHandler>().navigate(WithdrawStepsScreen.route);
+                  style: PassaquiButtonStyle.primary,
+                  showArrow: true,
+                  label: "Veja como habilitar",
+                  borderColor: Colors.black,
+                  onTap: () {
+                    DIService()
+                        .inject<NavigationHandler>()
+                        .navigate(WithdrawStepsScreen.route);
                   },
                 ),
-        
               ],
             ),
           ),
