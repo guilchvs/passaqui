ca7f8bf3d133bffa77caf65293c8483ba34be7a9
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-19 15:10:41 -0300

    fix: fix input text

--- lib/src/modules/hire/confirmCpf/confirm_cpf_screen.dart
+++ lib/src/modules/hire/confirmCpf/confirm_cpf_screen.dart
@@ -1,320 +1,189 @@
- import 'package:flutter/material.dart';
- import 'package:google_fonts/google_fonts.dart';
-import 'package:passaqui/src/modules/hire/confirmEmail/hire_confirm_email.dart';
- import 'package:passaqui/src/shared/widget/appbar.dart';
- import 'package:passaqui/src/shared/widget/button.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 import '../../../core/di/service_locator.dart';
-import '../../../core/navigation/navigation_handler.dart'; // Assuming PassaquiButton is imported from here
+import '../../../core/navigation/navigation_handler.dart';
+import '../../../shared/widget/appbar.dart';
+import '../../../shared/widget/button.dart';
+import '../confirmEmail/hire_confirm_email.dart';
 
- class HireConfirmCpfScreen extends StatefulWidget {
-   static const String route = "/hire-confirm-cpf";
-   final String? cpf;
+class HireConfirmCpfScreen extends StatefulWidget {
+  static const String route = "/hire-confirm-cpf";
+  final String? cpf;
 
-   const HireConfirmCpfScreen({Key? key, this.cpf}) : super(key: key);
+  const HireConfirmCpfScreen({Key? key, this.cpf}) : super(key: key);
 
-   @override
-   State<HireConfirmCpfScreen> createState() => _HireConfirmCpfScreenState();
- }
+  @override
+  State<HireConfirmCpfScreen> createState() => _HireConfirmCpfScreenState();
+}
 
- class _HireConfirmCpfScreenState extends State<HireConfirmCpfScreen> {
-   TextEditingController _firstPartController = TextEditingController();
-   TextEditingController _secondPartController = TextEditingController();
-   TextEditingController _thirdPartController = TextEditingController();
+class _HireConfirmCpfScreenState extends State<HireConfirmCpfScreen> {
+  late TextEditingController _firstPartController;
+  late TextEditingController _secondPartController;
+  late TextEditingController _thirdPartController;
 
-   @override
-   void initState() {
-     super.initState();
-   }
+  late FocusNode _firstFocusNode;
+  late FocusNode _secondFocusNode;
+  late FocusNode _thirdFocusNode;
 
-   @override
-   Widget build(BuildContext context) {
-     return Scaffold(
-       appBar: const PassaquiAppBar(
-         showLogo: false,
-         showBackButton: true,
-       ),
-       backgroundColor: Color.fromRGBO(18, 96, 73, 1),
-       body: Stack(
-         children: [
-           Column(
-             crossAxisAlignment: CrossAxisAlignment.stretch,
-             children: [
-               Padding(
-                 padding: const EdgeInsets.only(left: 24.0, top: 16),
-                 child: Text(
-                   'Informações básicas',
-                   style: GoogleFonts.roboto(
-                     color: Colors.white,
-                     fontSize: 20,
-                     fontWeight: FontWeight.w500,
-                   ),
-                 ),
-               ),
-               SizedBox(height: 24),
-               Expanded(
-                 child: Container(
-                   color: Colors.white,
-                   padding: EdgeInsets.all(16),
-                   child: Column(
-                     crossAxisAlignment: CrossAxisAlignment.start,
-                     children: [
-                       SizedBox(height: 16),
-                       Center(
-                         child: Text(
-                           'Confirme os 3 primeiros dígitos do seu CPF:',
-                           style: GoogleFonts.roboto(
-                             fontWeight: FontWeight.w500,
-                             color: Color(0xFF515151),
-                             fontSize: 16,
-                           ),
-                         ),
-                       ),
-                       SizedBox(height: 24),
-                       Row(
-                         mainAxisAlignment: MainAxisAlignment.center,
-                         children: [
-                           _buildInputField(_firstPartController),
-                           SizedBox(width: 2),
-                           _buildInputField(_secondPartController),
-                           SizedBox(width: 2),
-                           _buildInputField(_thirdPartController),
-                           SizedBox(width: 2),
-                           _buildNumberText(widget.cpf != null && widget.cpf!.length >= 6 ? '.${widget.cpf!.substring(3, 6)}' : '.'),
-                           SizedBox(width: 2),
-                           _buildNumberText(widget.cpf != null && widget.cpf!.length >= 9 ? '.${widget.cpf!.substring(6, 9)}' : '.'),
-                           SizedBox(width: 2),
-                           _buildNumberText(widget.cpf != null && widget.cpf!.length >= 11 ? '-${widget.cpf!.substring(9)}' : '-'),
-                         ],
-                       ),
-                       SizedBox(height: 48),
-                       Center(
-                         child: PassaquiButton(
-                           label: 'Confirmar',
-                           centerText: true,
-                           borderRadius: 50,
-                           style: PassaquiButtonStyle.primary,
-                           onTap: () {
-                             DIService().inject<NavigationHandler>().navigate(
-                               HireConfirmEmailScreen.route,
-                             );
-                           },
-                         ),
-                       ),
-                     ],
-                   ),
-                 ),
-               ),
-             ],
-           ),
-         ],
-       ),
-     );
-   }
+  @override
+  void initState() {
+    super.initState();
+    _firstPartController = TextEditingController();
+    _secondPartController = TextEditingController();
+    _thirdPartController = TextEditingController();
 
-   Widget _buildNumberText(String text) {
-     return Text(
-       text,
-       style: GoogleFonts.roboto(
-         fontWeight: FontWeight.w400,
-         color: Color(0xFF515151),
-         fontSize: 40,
-       ),
-     );
-   }
+    _firstFocusNode = FocusNode();
+    _secondFocusNode = FocusNode();
+    _thirdFocusNode = FocusNode();
+  }
 
-   Widget _buildInputField(TextEditingController controller) {
-     return Container(
-       width: 26,
-       height: 40,
-       child: TextField(
-         controller: controller,
-         textAlign: TextAlign.center,
-         keyboardType: TextInputType.number,
-         maxLength: 1,
-         style: GoogleFonts.roboto(
-           fontWeight: FontWeight.w400,
-           color: Colors.black,
-           fontSize: 12,
-         ),
-         decoration: InputDecoration(
-           filled: true,
-           fillColor: Color(0xFFF2F2F2),
-           counterText: "",
-           border: OutlineInputBorder(
-             borderSide: BorderSide.none,
-             borderRadius: BorderRadius.circular(8),
-           ),
-         ),
-       ),
-     );
-   }
- }
+  @override
+  void dispose() {
+    _firstPartController.dispose();
+    _secondPartController.dispose();
+    _thirdPartController.dispose();
+    _firstFocusNode.dispose();
+    _secondFocusNode.dispose();
+    _thirdFocusNode.dispose();
+    super.dispose();
+  }
 
+  void _handleTextChange(TextEditingController controller, FocusNode currentFocus, FocusNode? nextFocus, [FocusNode? previousFocus]) {
+    String text = controller.text;
+    if (text.isNotEmpty && nextFocus != null) {
+      // Move focus to the next field
+      FocusScope.of(context).requestFocus(nextFocus);
+    } else if (text.isEmpty && previousFocus != null) {
+      // Move focus to the previous field when text is deleted
+      FocusScope.of(context).requestFocus(previousFocus);
+    }
+  }
 
-//import 'package:flutter/material.dart';
-//import 'package:google_fonts/google_fonts.dart';
-//import 'package:passaqui/src/shared/widget/appbar.dart';
-//import 'package:passaqui/src/shared/widget/button.dart'; // Assuming PassaquiButton is imported from here
-//
-//class HireConfirmCpfScreen extends StatefulWidget {
-//  static const String route = "/hire-confirm-cpf";
-//  final String? cpf;
-//
-//  const HireConfirmCpfScreen({Key? key, this.cpf}) : super(key: key);
-//
-//  @override
-//  State<HireConfirmCpfScreen> createState() => _HireConfirmCpfScreenState();
-//}
-//
-//class _HireConfirmCpfScreenState extends State<HireConfirmCpfScreen> {
-//  TextEditingController _firstPartController = TextEditingController();
-//  TextEditingController _secondPartController = TextEditingController();
-//  TextEditingController _thirdPartController = TextEditingController();
-//
-//  @override
-//  void initState() {
-//    super.initState();
-//  }
-//
-//  @override
-//  Widget build(BuildContext context) {
-//    return Scaffold(
-//      appBar: const PassaquiAppBar(
-//        showLogo: false,
-//        showBackButton: true,
-//      ),
-//      backgroundColor: Color.fromRGBO(18, 96, 73, 1),
-//      body: Stack(
-//        children: [
-//          Column(
-//            crossAxisAlignment: CrossAxisAlignment.stretch,
-//            children: [
-//              Padding(
-//                padding: const EdgeInsets.only(left: 24.0, top: 16),
-//                child: Text(
-//                  'Informações básicas',
-//                  style: GoogleFonts.roboto(
-//                    color: Colors.white,
-//                    fontSize: 20,
-//                    fontWeight: FontWeight.w500,
-//                  ),
-//                ),
-//              ),
-//              SizedBox(height: 24),
-//              Expanded(
-//                child: Container(
-//                  color: Colors.white,
-//                  padding: EdgeInsets.all(16),
-//                  child: Column(
-//                    crossAxisAlignment: CrossAxisAlignment.start,
-//                    children: [
-//                      SizedBox(height: 16),
-//                      Center(
-//                        child: Text(
-//                          'Confirme os 3 primeiros dígitos do seu CPF:',
-//                          style: GoogleFonts.roboto(
-//                            fontWeight: FontWeight.w500,
-//                            color: Color(0xFF515151),
-//                            fontSize: 16,
-//                          ),
-//                        ),
-//                      ),
-//                      SizedBox(height: 24),
-//                      Row(
-//                        mainAxisAlignment: MainAxisAlignment.center,
-//                        children: [
-//                          _buildInputField(_firstPartController),
-//                          SizedBox(width: 2),
-//                          _buildInputField(_secondPartController),
-//                          SizedBox(width: 2),
-//                          _buildInputField(_thirdPartController),
-//                          SizedBox(width: 2),
-//                          _buildNumberText(widget.cpf != null && widget.cpf!.length >= 6 ? '.${widget.cpf!.substring(3, 6)}' : '.'),
-//                          SizedBox(width: 2),
-//                          _buildNumberText(widget.cpf != null && widget.cpf!.length >= 9 ? '.${widget.cpf!.substring(6, 9)}' : '.'),
-//                          SizedBox(width: 2),
-//                          _buildNumberText(widget.cpf != null && widget.cpf!.length >= 11 ? '-${widget.cpf!.substring(9)}' : '-'),
-//                        ],
-//                      ),
-//                      SizedBox(height: 48),
-//                      Center(
-//                        child: PassaquiButton(
-//                          label: 'Confirmar',
-//                          centerText: true,
-//                          borderRadius: 50,
-//                          style: PassaquiButtonStyle.primary,
-//                          onTap: () {
-//                            // Print the values of the inputs
-//                            String firstPart = _firstPartController.text;
-//                            String secondPart = _secondPartController.text;
-//                            String thirdPart = _thirdPartController.text;
-//
-//                            print('Primeiro dígito: $firstPart');
-//                            print('Segundo dígito: $secondPart');
-//                            print('Terceiro dígito: $thirdPart');
-//                          },
-//                        ),
-//                      ),
-//                    ],
-//                  ),
-//                ),
-//              ),
-//            ],
-//          ),
-//        ],
-//      ),
-//    );
-//  }
-//
-//  Widget _buildNumberText(String text) {
-//    return Text(
-//      text,
-//      style: GoogleFonts.roboto(
-//        fontWeight: FontWeight.w400,
-//        color: Color(0xFF515151),
-//        fontSize: 40,
-//      ),
-//    );
-//  }
-//
-//  Widget _buildInputField(TextEditingController controller) {
-//    return Container(
-//      width: 40, // Adjust width as needed
-//      height: 40, // Adjust height as needed
-//      child: TextField(
-//        controller: controller,
-//        textAlignVertical: TextAlignVertical.bottom, // Align text to the bottom
-//        textAlign: TextAlign.center, // Center align horizontally
-//        keyboardType: TextInputType.number,
-//        maxLength: 1,
-//        onChanged: (value) {
-//          // Update the controller's text
-//          controller.text = value;
-//          // Move cursor to the end of the text
-//          controller.selection = TextSelection.fromPosition(TextPosition(offset: controller.text.length));
-//        },
-//        style: GoogleFonts.roboto(
-//          fontWeight: FontWeight.w400,
-//          color: Color(0xFF515151), // Same color as CPF numbers
-//          fontSize: 40, // Same font size as CPF numbers
-//        ),
-//        decoration: InputDecoration(
-//          filled: true,
-//          fillColor: Color(0xFFF2F2F2),
-//          counterText: "",
-//          contentPadding: EdgeInsets.only(bottom: 8), // Adjust padding to align text
-//          border: OutlineInputBorder(
-//            borderSide: BorderSide.none,
-//            borderRadius: BorderRadius.circular(8),
-//          ),
-//          prefix: SizedBox(height: 40, child: Container()), // Empty container for height
-//          prefixIconConstraints: BoxConstraints(
-//            minWidth: 0, // Min width to make it work
-//          ),
-//        ),
-//      ),
-//    );
-//  }
-//}
-//
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const PassaquiAppBar(
+        showLogo: false,
+        showBackButton: true,
+      ),
+      backgroundColor: Color.fromRGBO(18, 96, 73, 1),
+      body: Stack(
+        children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Padding(
+                padding: const EdgeInsets.only(left: 24.0, top: 16),
+                child: Text(
+                  'Informações básicas',
+                  style: GoogleFonts.roboto(
+                    color: Colors.white,
+                    fontSize: 20,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+              SizedBox(height: 24),
+              Expanded(
+                child: Container(
+                  color: Colors.white,
+                  padding: EdgeInsets.all(16),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      SizedBox(height: 16),
+                      Center(
+                        child: Text(
+                          'Confirme os 3 primeiros dígitos do seu CPF:',
+                          style: GoogleFonts.roboto(
+                            fontWeight: FontWeight.w500,
+                            color: Color(0xFF515151),
+                            fontSize: 16,
+                          ),
+                        ),
+                      ),
+                      SizedBox(height: 24),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          _buildInputField(_firstPartController, _firstFocusNode, _secondFocusNode),
+                          SizedBox(width: 2),
+                          _buildInputField(_secondPartController, _secondFocusNode, _thirdFocusNode, _firstFocusNode),
+                          SizedBox(width: 2),
+                          _buildInputField(_thirdPartController, _thirdFocusNode, null, _secondFocusNode),
+                          SizedBox(width: 2),
+                          _buildNumberText(widget.cpf != null && widget.cpf!.length >= 6 ? '.${widget.cpf!.substring(3, 6)}' : '.'),
+                          SizedBox(width: 2),
+                          _buildNumberText(widget.cpf != null && widget.cpf!.length >= 9 ? '.${widget.cpf!.substring(6, 9)}' : '.'),
+                          SizedBox(width: 2),
+                          _buildNumberText(widget.cpf != null && widget.cpf!.length >= 11 ? '-${widget.cpf!.substring(9)}' : '-'),
+                        ],
+                      ),
+                      SizedBox(height: 48),
+                      Center(
+                        child: PassaquiButton(
+                          label: 'Confirmar',
+                          centerText: true,
+                          borderRadius: 50,
+                          style: PassaquiButtonStyle.primary,
+                          onTap: () {
+                            DIService().inject<NavigationHandler>().navigate(
+                              HireConfirmEmailScreen.route,
+                            );
+                          },
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildNumberText(String text) {
+    return Text(
+      text,
+      style: GoogleFonts.roboto(
+        fontWeight: FontWeight.w400,
+        color: Color(0xFF515151),
+        fontSize: 40,
+      ),
+    );
+  }
+
+  Widget _buildInputField(TextEditingController controller, FocusNode focusNode, FocusNode? nextFocus, [FocusNode? previousFocus]) {
+    return Container(
+      width: 50,
+      height: 60,
+      child: TextField(
+        controller: controller,
+        focusNode: focusNode,
+        textAlign: TextAlign.center,
+        keyboardType: TextInputType.number,
+        maxLength: 1,
+        onChanged: (_) {
+          _handleTextChange(controller, focusNode, nextFocus, previousFocus);
+        },
+        style: GoogleFonts.roboto(
+          fontWeight: FontWeight.w400,
+          color: Colors.black,
+          fontSize: 24,
+        ),
+        decoration: InputDecoration(
+          filled: true,
+          fillColor: Color(0xFFF2F2F2),
+          counterText: "",
+          border: OutlineInputBorder(
+            borderSide: BorderSide.none,
+            borderRadius: BorderRadius.circular(8),
+          ),
+        ),
+      ),
+    );
+  }
+}
