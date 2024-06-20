a1f906a8c86d1bcf74c9602c7e80bc65997a027b
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-20 17:44:52 -0300

    feat: update textButton and add fix button rendering

--- lib/src/modules/withdraw/steps/withdraw_step_screen.dart
+++ lib/src/modules/withdraw/steps/withdraw_step_screen.dart
@@ -7,7 +7,7 @@ import 'package:passaqui/src/shared/widget/text_button.dart';
 class WithdrawStepsScreen extends StatefulWidget {
   static const String route = "/withdraw-how-to";
 
-  const WithdrawStepsScreen({super.key});
+  const WithdrawStepsScreen({Key? key}) : super(key: key);
 
   @override
   State<WithdrawStepsScreen> createState() => _WithdrawStepsScreenState();
@@ -23,126 +23,125 @@ class _WithdrawStepsScreenState extends State<WithdrawStepsScreen> {
   }
 
   List<WithdrawStepItem> steps = [
-    const WithdrawStepItem(
+    WithdrawStepItem(
       step: 1,
       title: "1. Libere o seu Saque-Aniversário",
       image: "assets/images/withdraw/withdraw_one.png",
       description:
-          "Para liberar o Saque aniversário é preciso que tenha o aplicativo do FGTS, baixe pelo App store ou google store do seu celular. \n\n- Se você ainda não liberou a modalidade Saque-Aniversário no aplicativo FGTS, preparamos um passo a passo para te apoiar.\nClique em entrar no aplicativo \n- Caso já tenha a modalidade liberada, falta apenas liberar nosso banco, clique aqui e veja como autorizar.",
+      "Para liberar o Saque aniversário é preciso que tenha o aplicativo do FGTS, baixe pelo App store ou google store do seu celular. \n\n- Se você ainda não liberou a modalidade Saque-Aniversário no aplicativo FGTS, preparamos um passo a passo para te apoiar.\nClique em entrar no aplicativo \n- Caso já tenha a modalidade liberada, falta apenas liberar nosso banco, clique aqui e veja como autorizar.",
     ),
-    const WithdrawStepItem(
+    WithdrawStepItem(
       step: 2,
       title: "2. Logue no aplicativo do FGTS",
       image: "assets/images/withdraw/withdraw_two.png",
       description:
-          "- Digite seus dados e clique em entrar; \n- Caso seja seu primeiro acesso, clique em cadastre-se e siga o passo a passo da Caixa.",
+      "- Digite seus dados e clique em entrar; \n- Caso seja seu primeiro acesso, clique em cadastre-se e siga o passo a passo da Caixa.",
     ),
-    const WithdrawStepItem(
+    WithdrawStepItem(
       step: 3,
       title: "3. No menu inicial",
       image: "assets/images/withdraw/withdraw_three.png",
       description: "Clique no saque aniversário.",
     ),
-    const WithdrawStepItem(
+    WithdrawStepItem(
       step: 4,
       title: "4. Confirme em autorizar e optar pelo Saque-Aniversário",
       image: "assets/images/withdraw/withdraw_four.png",
       description:
-          "Clique no aceite e optar pelo saque aniversário. \n\nConfirme e clique em continuar, a caixa vai pedir para cadastrar seu banco e opções de como quer sacar. Siga os passos indicados até a sua solicitação ser processada com sucesso!",
+      "Clique no aceite e optar pelo saque aniversário. \n\nConfirme e clique em continuar, a caixa vai pedir para cadastrar seu banco e opções de como quer sacar. Siga os passos indicados até a sua solicitação ser processada com sucesso!",
     ),
-    const WithdrawStepItem(
+    WithdrawStepItem(
       step: 5,
       title: "5. Autorizando o banco BMP",
       image: "assets/images/withdraw/withdraw_five.png",
       description:
-          "- Tudo certo com a opção do Saque FGTS, você precisa autorizar o banco a consultar seus dados e limite liberado pela caixa; \n- Novamente no menu principal, clique no botão Autorizar bancos a consultarem seu FGTS.",
+      "- Tudo certo com a opção do Saque FGTS, você precisa autorizar o banco a consultar seus dados e limite liberado pela caixa; \n- Novamente no menu principal, clique no botão Autorizar bancos a consultarem seu FGTS.",
     ),
-    const WithdrawStepItem(
+    WithdrawStepItem(
       step: 6,
       title: "6. Autorize o Saque-Aniversário",
       image: "assets/images/withdraw/withdraw_six.png",
       description:
-          "- Clique em Empréstimo Saque Aniversário na seta que indica a direita; \n- Na sequência clique em visualizar o termo;\n- Clique no aceite e no botão continuar.",
+      "- Clique em Empréstimo Saque Aniversário na seta que indica a direita; \n- Na sequência clique em visualizar o termo;\n- Clique no aceite e no botão continuar.",
     ),
-    const WithdrawStepItem(
+    WithdrawStepItem(
       step: 7,
       title: "7. Procure o Banco BMP Sociedade de crédito direto S.A",
       image: "assets/images/withdraw/withdraw_seven.png",
       description:
-          "- Digite BMP em buscar banco; \n- Seleciona o Banco BMP;\n- Clique em continuar;\n- Clique em confirmar seleção.\n\nPronto, você autorizou o BMP!\nDentro de 24 horas entre no Aplicativo e solicite seu empréstimo!",
+      "- Digite BMP em buscar banco; \n- Seleciona o Banco BMP;\n- Clique em continuar;\n- Clique em confirmar seleção.\n\nPronto, você autorizou o BMP!\nDentro de 24 horas entre no Aplicativo e solicite seu empréstimo!",
     ),
   ];
-
-  int getCurrentPageIndex() {
-    return _pageController.hasClients
-        ? (_pageController.page?.toInt() ?? 0)
-        : 0;
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
         children: [
-          const SizedBox(
-            height: 32,
-          ),
-          const Text(
+          SizedBox(height: 32),
+          Text(
             "Como realizar a contratação?",
             style: TextStyle(
-                fontSize: 22,
-                color: Color(0xFF1E1E1E),
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w700),
+              fontSize: 22,
+              color: Color(0xFF1E1E1E),
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w700,
+            ),
           ),
-          const SizedBox(
-            height: 22,
-          ),
+          SizedBox(height: 22),
           Expanded(
-              child: PageView.builder(
-                  controller: _pageController,
-                  itemCount: steps.length,
-                  itemBuilder: (context, index) {
-                    return steps[index];
-                  })),
+            child: PageView.builder(
+              controller: _pageController,
+              itemCount: steps.length,
+              itemBuilder: (context, index) {
+                return steps[index];
+              },
+            ),
+          ),
           SafeArea(
-              child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 24),
-            child: Row(
-              children: [
-                if (getCurrentPageIndex() > 0)
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 78, vertical: 48),
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
                   PassaquiTextButton(
                     isLeading: true,
                     label: "Anterior",
+                    textColor: Color(0xFF1E1E1E),
+                    arrowColor: Color(0xFF126049),
                     onTap: () {
-                      _pageController
-                          .jumpToPage((_pageController.page?.toInt() ?? 0) - 1);
-                    },
-                  ),
-                const Spacer(),
-
-                Padding(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 35
-                    ),
-                  child: PassaquiTextButton(
-                    label: "Próxima",
-                    onTap: () {
-                      int? lastPage = _pageController.page?.toInt();
-                      if (lastPage == steps.length - 1) {
-                        DIService()
-                            .inject<NavigationHandler>()
-                            .navigate(HomeScreen.route);
-                      } else {
-                        _pageController
-                            .jumpToPage((_pageController.page?.toInt() ?? 0) + 1);
+                      if (_pageController.page?.toInt() != 0) {
+                        _pageController.previousPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                        );
                       }
                     },
                   ),
-                )
-              ],
+                    PassaquiTextButton(
+                      label: "Próxima",
+                      textColor: Color(0xFF1E1E1E),
+                      fontSize: 22,
+                      fontWeight: FontWeight.w500,
+                      arrowColor: Color(0xFF126049),
+                      onTap: () {
+                        int? lastPage = _pageController.page?.toInt();
+                        if (lastPage == steps.length - 1) {
+                          DIService()
+                              .inject<NavigationHandler>()
+                              .navigate(HomeScreen.route);
+                        } else {
+                          _pageController.nextPage(
+                            duration: const Duration(milliseconds: 500),
+                            curve: Curves.easeInOut,
+                          );
+                        }
+                      },
+                    ),
+                ],
+              ),
             ),
-          ))
+          ),
         ],
       ),
     );
@@ -155,12 +154,13 @@ class WithdrawStepItem extends StatelessWidget {
   final String image;
   final String description;
 
-  const WithdrawStepItem(
-      {required this.step,
-      required this.title,
-      required this.image,
-      required this.description,
-      super.key});
+  const WithdrawStepItem({
+    required this.step,
+    required this.title,
+    required this.image,
+    required this.description,
+    Key? key,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -172,39 +172,39 @@ class WithdrawStepItem extends StatelessWidget {
           Text(
             "$step.",
             style: const TextStyle(
-                fontSize: 22,
-                color: Color(0xFF136048),
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w700),
+              fontSize: 22,
+              color: Color(0xFF136048),
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w700,
+            ),
           ),
           Text(
             title,
             style: const TextStyle(
-                fontSize: 20,
-                color: Color(0xFF136048),
-                fontFamily: 'Raleway',
-                fontWeight: FontWeight.w700),
+              fontSize: 20,
+              color: Color(0xFF136048),
+              fontFamily: 'Raleway',
+              fontWeight: FontWeight.w700,
+            ),
           ),
-          const SizedBox(
-            height: 32,
-          ),
+          const SizedBox(height: 32),
           Image.asset(
             image,
             height: 270,
             width: double.infinity,
             fit: BoxFit.contain,
           ),
-          const SizedBox(
-            height: 10,
-          ),
+          const SizedBox(height: 10),
           RichText(
-              text: TextSpan(
-                  style: const TextStyle(
-                    fontSize: 16,
-                    color: Color(0xFF1E1E1E),
-                    fontFamily: 'Raleway',
-                  ),
-                  children: _buildDescription(description, step)))
+            text: TextSpan(
+              style: const TextStyle(
+                fontSize: 16,
+                color: Color(0xFF1E1E1E),
+                fontFamily: 'Raleway',
+              ),
+              children: _buildDescription(description, step),
+            ),
+          ),
         ],
       ),
     );
@@ -247,3 +247,4 @@ class WithdrawStepItem extends StatelessWidget {
     return textSpans;
   }
 }
+
--- lib/src/shared/widget/text_button.dart
+++ lib/src/shared/widget/text_button.dart
@@ -1,16 +1,87 @@
+// import 'package:flutter/material.dart';
+//
+// class PassaquiTextButton extends StatelessWidget {
+//   final String label;
+//   final Function()? onTap;
+//   final bool isLeading;
+//   final Color textColor;
+//   final String fontFamily;
+//   final double fontSize;
+//   final FontWeight fontWeight;
+//
+//   const PassaquiTextButton({
+//     Key? key,
+//     required this.label,
+//     this.onTap,
+//     this.isLeading = false,
+//     this.textColor = const Color(0xFF5D5D5DB2),
+//     this.fontFamily = 'Raleway',
+//     this.fontSize = 16,
+//     this.fontWeight = FontWeight.w600,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onTap: onTap,
+//       child: Row(
+//         children: [
+//           isLeading
+//               ? Icon(
+//             Icons.arrow_back_ios,
+//             color: Color(0xFFA8CA4B),
+//             size: 18,
+//           )
+//               : const SizedBox(),
+//           Padding(
+//             padding: const EdgeInsets.symmetric(horizontal: 12),
+//             child: Text(
+//               label,
+//               style: TextStyle(
+//                 fontFamily: fontFamily,
+//                 fontSize: fontSize,
+//                 fontWeight: fontWeight,
+//                 color: textColor,
+//               ),
+//             ),
+//           ),
+//           isLeading
+//               ? const SizedBox()
+//               : Icon(
+//             Icons.arrow_forward_ios,
+//             color: Color(0xFFA8CA4B),
+//             size: 18,
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 
 class PassaquiTextButton extends StatelessWidget {
   final String label;
   final Function()? onTap;
   final bool isLeading;
+  final Color textColor;
+  final String fontFamily;
+  final double fontSize;
+  final FontWeight fontWeight;
+  final Color arrowColor;
 
   const PassaquiTextButton({
-    super.key,
+    Key? key,
     required this.label,
-    this.isLeading = false,
     this.onTap,
-  });
+    this.isLeading = false,
+    this.textColor = const Color(0xFF5D5D5DB2),
+    this.fontFamily = 'Raleway',
+    this.fontSize = 16,
+    this.fontWeight = FontWeight.w600,
+    this.arrowColor = const Color(0xFFA8CA4B),
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -20,29 +91,30 @@ class PassaquiTextButton extends StatelessWidget {
         children: [
           isLeading
               ? Icon(
-                  Icons.arrow_back_ios,
-                  color: Color(0xFFA8CA4B),
-                  size: 18,
-                )
+            Icons.arrow_back_ios,
+            color: arrowColor,
+            size: 18,
+          )
               : const SizedBox(),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 12),
             child: Text(
               label,
               style: TextStyle(
-                  fontFamily: 'Raleway',
-                  fontSize: 16,
-                  fontWeight: FontWeight.w600,
-                  color: Color(0xFF5D5D5DB2)),
+                fontFamily: fontFamily,
+                fontSize: fontSize,
+                fontWeight: fontWeight,
+                color: textColor,
+              ),
             ),
           ),
           isLeading
               ? const SizedBox()
               : Icon(
-                  Icons.arrow_forward_ios,
-                  color: Color(0xFFA8CA4B),
-                  size: 18,
-                ),
+            Icons.arrow_forward_ios,
+            color: arrowColor,
+            size: 18,
+          ),
         ],
       ),
     );
