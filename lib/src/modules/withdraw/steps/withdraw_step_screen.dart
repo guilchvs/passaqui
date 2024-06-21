a25363cb7b25dedb871e0d949485ab01c779c2b4
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-21 13:57:35 -0300

    chore: inicia jornada

--- lib/src/modules/withdraw/steps/withdraw_step_screen.dart
+++ lib/src/modules/withdraw/steps/withdraw_step_screen.dart
@@ -24,52 +24,55 @@ class _WithdrawStepsScreenState extends State<WithdrawStepsScreen> {
 
   List<WithdrawStepItem> steps = [
     WithdrawStepItem(
-      step: 1,
-      title: "1. Libere o seu Saque-Aniversário",
-      image: "assets/images/withdraw/withdraw_one.png",
-      description:
-      "Para liberar o Saque aniversário é preciso que tenha o aplicativo do FGTS, baixe pelo App store ou google store do seu celular. \n\n- Se você ainda não liberou a modalidade Saque-Aniversário no aplicativo FGTS, preparamos um passo a passo para te apoiar.\nClique em entrar no aplicativo \n- Caso já tenha a modalidade liberada, falta apenas liberar nosso banco, clique aqui e veja como autorizar.",
-    ),
+        step: 1,
+        title: "1. Libere o seu Saque-Aniversário",
+        image: "assets/images/withdraw/withdraw_one.png",
+        backgroundImage: "assets/images/withdraw/withdraw_back_logo.svg",
+        description:
+            "Preencha suas informações e aguarde nosso contato. Nossa equipe está à sua disposição para apoiar seus objetivos financeiros."),
     WithdrawStepItem(
       step: 2,
       title: "2. Logue no aplicativo do FGTS",
       image: "assets/images/withdraw/withdraw_two.png",
+      backgroundImage: "assets/images/withdraw/withdraw_back_logo.svg",
       description:
-      "- Digite seus dados e clique em entrar; \n- Caso seja seu primeiro acesso, clique em cadastre-se e siga o passo a passo da Caixa.",
+          "-Digite seus dados e clique em entrar;-Caso seja seu primeiro acesso, clique em cadastre-se e siga o passo a passo da Caixa.",
     ),
     WithdrawStepItem(
-      step: 3,
-      title: "3. No menu inicial",
-      image: "assets/images/withdraw/withdraw_three.png",
-      description: "Clique no saque aniversário.",
-    ),
+        step: 3,
+        title: "3. No menu inicial",
+        image: "assets/images/withdraw/withdraw_three.png",
+        backgroundImage: "assets/images/withdraw/withdraw_back_logo.svg",
+        description: "Clique na opção “Saque-Aniversário do FGTS”."),
     WithdrawStepItem(
-      step: 4,
-      title: "4. Confirme em autorizar e optar pelo Saque-Aniversário",
-      image: "assets/images/withdraw/withdraw_four.png",
-      description:
-      "Clique no aceite e optar pelo saque aniversário. \n\nConfirme e clique em continuar, a caixa vai pedir para cadastrar seu banco e opções de como quer sacar. Siga os passos indicados até a sua solicitação ser processada com sucesso!",
-    ),
+        step: 4,
+        title: "4. Confirme em autorizar e optar pelo Saque-Aniversário",
+        image: "assets/images/withdraw/withdraw_four.png",
+        backgroundImage: "assets/images/withdraw/withdraw_back_logo.svg",
+        description:
+            "Clique no aceite para optar pelo Saque-Aniversário. \n\nConfirme e clique em “Continuar”, a Caixa vai pedir para cadastrar seu banco e opções de como quer sacar. Siga os passos indicados até a sua solicitação ser processada com sucesso!"),
     WithdrawStepItem(
-      step: 5,
-      title: "5. Autorizando o banco BMP",
-      image: "assets/images/withdraw/withdraw_five.png",
-      description:
-      "- Tudo certo com a opção do Saque FGTS, você precisa autorizar o banco a consultar seus dados e limite liberado pela caixa; \n- Novamente no menu principal, clique no botão Autorizar bancos a consultarem seu FGTS.",
-    ),
+        step: 5,
+        title: "5. Autorizando o banco BMP",
+        image: "assets/images/withdraw/withdraw_five.png",
+        backgroundImage: "assets/images/withdraw/withdraw_back_logo.svg",
+        description:
+            "Tudo certo com a opção do Saque FGTS, você precisa autorizar o banco a consultar seus dados e limite liberado pela Caixa;\n\nNovamente no menu principal, clique no botão “Autorizar bancos a consultarem seu FGTS”."),
     WithdrawStepItem(
       step: 6,
       title: "6. Autorize o Saque-Aniversário",
       image: "assets/images/withdraw/withdraw_six.png",
+      backgroundImage: "assets/images/withdraw/withdraw_back_logo.svg",
       description:
-      "- Clique em Empréstimo Saque Aniversário na seta que indica a direita; \n- Na sequência clique em visualizar o termo;\n- Clique no aceite e no botão continuar.",
+          "• Clique em Empréstimo Saque Aniversário na seta que indica a direita; \n• Na sequência clique em visualizar o termo;\n• Clique no aceite e no botão continuar.",
     ),
     WithdrawStepItem(
       step: 7,
       title: "7. Procure o Banco BMP Sociedade de crédito direto S.A",
       image: "assets/images/withdraw/withdraw_seven.png",
+      backgroundImage: "assets/images/withdraw/withdraw_back_logo.svg",
       description:
-      "- Digite BMP em buscar banco; \n- Seleciona o Banco BMP;\n- Clique em continuar;\n- Clique em confirmar seleção.\n\nPronto, você autorizou o BMP!\nDentro de 24 horas entre no Aplicativo e solicite seu empréstimo!",
+          "• Digite BMP em buscar banco; \n• Seleciona o Banco BMP;\n• Clique em continuar;\n• Clique em confirmar seleção.\n\nPronto, você autorizou o BMP!\nDentro de 24 horas entre no Aplicativo e solicite seu empréstimo!",
     ),
   ];
 
@@ -78,16 +81,19 @@ class _WithdrawStepsScreenState extends State<WithdrawStepsScreen> {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(height: 32),
-          Text(
-            "Como realizar a contratação?",
-            style: TextStyle(
-              fontSize: 22,
-              color: Color(0xFF1E1E1E),
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w700,
-            ),
-          ),
+          SizedBox(height: 48),
+              Padding(
+                padding: const EdgeInsets.only(right: 16.0),
+                child: Text(
+                  "Como realizar a contratação?",
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.black,
+                    fontFamily: 'Raleway',
+                    fontWeight: FontWeight.w700,
+                  ),
+                ),
+              ),
           SizedBox(height: 22),
           Expanded(
             child: PageView.builder(
@@ -102,6 +108,7 @@ class _WithdrawStepsScreenState extends State<WithdrawStepsScreen> {
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 78, vertical: 48),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
                   PassaquiTextButton(
@@ -109,6 +116,7 @@ class _WithdrawStepsScreenState extends State<WithdrawStepsScreen> {
                     label: "Anterior",
                     textColor: Color(0xFF1E1E1E),
                     arrowColor: Color(0xFF126049),
+                    fontSize: 14,
                     onTap: () {
                       if (_pageController.page?.toInt() != 0) {
                         _pageController.previousPage(
@@ -118,26 +126,26 @@ class _WithdrawStepsScreenState extends State<WithdrawStepsScreen> {
                       }
                     },
                   ),
-                    PassaquiTextButton(
-                      label: "Próxima",
-                      textColor: Color(0xFF1E1E1E),
-                      fontSize: 22,
-                      fontWeight: FontWeight.w500,
-                      arrowColor: Color(0xFF126049),
-                      onTap: () {
-                        int? lastPage = _pageController.page?.toInt();
-                        if (lastPage == steps.length - 1) {
-                          DIService()
-                              .inject<NavigationHandler>()
-                              .navigate(HomeScreen.route);
-                        } else {
-                          _pageController.nextPage(
-                            duration: const Duration(milliseconds: 500),
-                            curve: Curves.easeInOut,
-                          );
-                        }
-                      },
-                    ),
+                  PassaquiTextButton(
+                    label: "Próxima",
+                    textColor: Color(0xFF1E1E1E),
+                    fontSize: 22,
+                    fontWeight: FontWeight.w500,
+                    arrowColor: Color(0xFF126049),
+                    onTap: () {
+                      int? lastPage = _pageController.page?.toInt();
+                      if (lastPage == steps.length - 1) {
+                        DIService()
+                            .inject<NavigationHandler>()
+                            .navigate(HomeScreen.route);
+                      } else {
+                        _pageController.nextPage(
+                          duration: const Duration(milliseconds: 500),
+                          curve: Curves.easeInOut,
+                        );
+                      }
+                    },
+                  ),
                 ],
               ),
             ),
@@ -152,12 +160,14 @@ class WithdrawStepItem extends StatelessWidget {
   final int step;
   final String title;
   final String image;
+  final String backgroundImage;
   final String description;
 
   const WithdrawStepItem({
     required this.step,
     required this.title,
     required this.image,
+    required this.backgroundImage,
     required this.description,
     Key? key,
   }) : super(key: key);
@@ -167,42 +177,37 @@ class WithdrawStepItem extends StatelessWidget {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 40),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
-          Text(
-            "$step.",
-            style: const TextStyle(
-              fontSize: 22,
-              color: Color(0xFF136048),
-              fontFamily: 'Raleway',
-              fontWeight: FontWeight.w700,
-            ),
-          ),
           Text(
             title,
             style: const TextStyle(
-              fontSize: 20,
-              color: Color(0xFF136048),
+              fontSize: 22,
+              color: Colors.black,
               fontFamily: 'Raleway',
-              fontWeight: FontWeight.w700,
+              fontWeight: FontWeight.w400,
             ),
           ),
-          const SizedBox(height: 32),
           Image.asset(
             image,
-            height: 270,
+            height: 300,
             width: double.infinity,
             fit: BoxFit.contain,
           ),
-          const SizedBox(height: 10),
-          RichText(
-            text: TextSpan(
-              style: const TextStyle(
-                fontSize: 16,
-                color: Color(0xFF1E1E1E),
-                fontFamily: 'Raleway',
+          Padding(
+            padding: const EdgeInsets.all(0),
+            child: Center(
+              child: RichText(
+                text: TextSpan(
+                  style: const TextStyle(
+                    fontSize: 18,
+                    color: Color(0xFF1E1E1E),
+                    fontFamily: 'Raleway',
+                    height: 1.4,
+                  ),
+                  children: _buildDescription(description, step),
+                ),
               ),
-              children: _buildDescription(description, step),
             ),
           ),
         ],
@@ -247,4 +252,3 @@ class WithdrawStepItem extends StatelessWidget {
     return textSpans;
   }
 }
-
