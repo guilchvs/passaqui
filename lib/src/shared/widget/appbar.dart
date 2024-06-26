1cdfebfab10121e2b14953886d6969d230f0a4b6
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-26 09:37:05 -0300

    feat: add webview to hire confirme email screen

--- lib/src/shared/widget/appbar.dart
+++ lib/src/shared/widget/appbar.dart
@@ -38,16 +38,57 @@
 //  Size get preferredSize => const Size.fromHeight(56.0);
 //}
 
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+//
+// class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   final bool showBackButton;
+//   final bool showLogo;
+//
+//   const PassaquiAppBar({
+//     this.showBackButton = false,
+//     this.showLogo = true,
+//     Key? key,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       key: key,
+//       elevation: 0,
+//       backgroundColor: Theme.of(context).primaryColor,
+//       automaticallyImplyLeading: showBackButton,
+//       title: showLogo
+//           ? Padding(
+//               padding: const EdgeInsets.only(left: 1.0),
+//               child: Image.asset(
+//                 "assets/images/logo_passaqui_hor_1.png",
+//                 fit: BoxFit.contain,
+//                 height: 100,
+//                 width: 90,
+//               ),
+//             )
+//           : null,
+//     );
+//   }
+//
+//   @override
+//   Size get preferredSize => const Size.fromHeight(56.0);
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool showBackButton;
   final bool showLogo;
+  final String? title;
 
   const PassaquiAppBar({
     this.showBackButton = false,
     this.showLogo = true,
+    this.title,
     Key? key,
   }) : super(key: key);
 
@@ -58,8 +99,10 @@ class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
       elevation: 0,
       backgroundColor: Theme.of(context).primaryColor,
       automaticallyImplyLeading: showBackButton,
-      title: showLogo
-          ? Padding(
+      title: Row(
+        children: [
+          if (showLogo)
+            Padding(
               padding: const EdgeInsets.only(left: 1.0),
               child: Image.asset(
                 "assets/images/logo_passaqui_hor_1.png",
@@ -67,8 +110,20 @@ class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
                 height: 100,
                 width: 90,
               ),
-            )
-          : null,
+            ),
+          if (title != null) // If title is provided, show it
+            Expanded(
+              child: Text(
+                title!,
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.w500,
+                ),
+                textAlign: TextAlign.start,
+              ),
+            ),
+        ],
+      ),
     );
   }
 
