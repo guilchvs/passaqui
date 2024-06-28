ea5f23c26153ff7090c10a4ed120a3249f54f6d9
Guilherme Chaves <guilherme.chaves@simplasolucoes.com.br>
2024-06-28 11:33:00 -0300

    chore: adiciona observable

--- lib/src/modules/hire/confirmEmail/hire_confirm_email.dart
+++ lib/src/modules/hire/confirmEmail/hire_confirm_email.dart
@@ -1,256 +1,3 @@
-// import 'package:flutter/material.dart';
-// import 'package:google_fonts/google_fonts.dart';
-// import 'package:http/http.dart' as http;
-// import 'package:passaqui/src/shared/widget/appbar.dart';
-// import 'package:passaqui/src/shared/widget/button.dart';
-// import 'package:url_launcher/url_launcher_string.dart';
-//
-// import '../../../core/di/service_locator.dart';
-// import '../../../core/navigation/navigation_handler.dart';
-// import '../../../services/auth_service.dart';
-// import '../wrongInfo/wrong_cpf_screen.dart';
-//
-// class HireConfirmEmailScreen extends StatefulWidget {
-//   static const String route = "/hire-confirm-email";
-//   final String? cpf;
-//
-//   const HireConfirmEmailScreen({Key? key, this.cpf}) : super(key: key);
-//
-//   @override
-//   State<HireConfirmEmailScreen> createState() => _HireConfirmEmailScreenState();
-// }
-//
-// class _HireConfirmEmailScreenState extends State<HireConfirmEmailScreen> {
-//   bool useOtherEmail = false; // Track the state of the radio buttons
-//   final AuthService _authService = AuthService();
-//   bool _isLoading = false;
-//   Map<String, dynamic>? _jsonResponse;
-//   String? userEmail; // Variable to hold the user's email
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     _fetchUserEmail(); // Fetch user's email when screen initializes
-//   }
-//
-//   Future<void> _fetchUserEmail() async {
-//     // Fetch user's email from SharedPreferences
-//     userEmail = await _authService.getEmail();
-//     setState(() {
-//       // Trigger a UI update
-//     });
-//   }
-//
-//   Future<void> _simulateApiCall(String? cpf) async {
-//     setState(() {
-//       _isLoading = true; // Set loading state while waiting for API response
-//     });
-//
-//     final baseUrl =
-//         'http://passcash-api-hml.us-east-1.elasticbeanstalk.com'; // Replace with your API base URL
-//     final token = await _authService.getToken(); // Retrieve JWT token
-//     final url = Uri.parse('$baseUrl/api/ApiMaster/iniciarBiometria?cpf=$cpf');
-//
-//     try {
-//       final response = await http.post(
-//         url,
-//         headers: <String, String>{
-//           'Content-Type': 'application/json; charset=UTF-8',
-//           'Authorization': 'Bearer $token', // Include Bearer token in headers
-//         },
-//       );
-//
-//       if (response.statusCode == 200) {
-//         final _url = response.body;
-//         await launchUrlString(_url);
-//       } else {
-//         // Handle other status codes here
-//         print('Failed to load data: ${response.statusCode}');
-//       }
-//     } catch (e) {
-//       // Handle exceptions
-//       print('Error: $e');
-//     } finally {
-//       setState(() {
-//         _isLoading = false; // Reset loading state
-//       });
-//     }
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: const PassaquiAppBar(
-//         showLogo: false,
-//         showBackButton: true,
-//       ),
-//       backgroundColor: Color.fromRGBO(18, 96, 73, 1),
-//       body: Stack(
-//         children: [
-//           Column(
-//             crossAxisAlignment: CrossAxisAlignment.stretch,
-//             children: [
-//               Padding(
-//                 padding: const EdgeInsets.only(left: 24.0, top: 16),
-//                 child: Text(
-//                   'Informações de contato',
-//                   style: GoogleFonts.roboto(
-//                     color: Colors.white,
-//                     fontSize: 20,
-//                     fontWeight: FontWeight.w500,
-//                   ),
-//                 ),
-//               ),
-//               SizedBox(height: 24),
-//               Expanded(
-//                 child: Container(
-//                   color: Colors.white,
-//                   padding: EdgeInsets.all(16),
-//                   child: Column(
-//                     crossAxisAlignment: CrossAxisAlignment.start,
-//                     children: [
-//                       SizedBox(height: 16),
-//                       Padding(
-//                         padding: const EdgeInsets.only(left: 16.0),
-//                         // Adjust left padding as needed
-//                         child: Text(
-//                           'Email atual',
-//                           style: GoogleFonts.roboto(
-//                             fontWeight: FontWeight.w600,
-//                             color: Color(0xFF515151),
-//                             fontSize: 18,
-//                           ),
-//                         ),
-//                       ),
-//                       SizedBox(height: 46),
-//                       _buildRadioOption(userEmail ?? ''),
-//                       // Display user's masked email
-//                       SizedBox(height: 54),
-//                       Center(
-//                         child: PassaquiButton(
-//                           label: 'Confirmar',
-//                           centerText: true,
-//                           borderRadius: 50,
-//                           style: PassaquiButtonStyle.primary,
-//                           onTap: () {
-//                             _simulateApiCall(widget.cpf);
-//                             // Handle button tap
-//                           },
-//                         ),
-//                       ),
-//                       SizedBox(height: 28),
-//                       Center(
-//                           child: GestureDetector(
-//                               onTap: () {
-//                                 DIService().inject<NavigationHandler>().navigate(
-//                                   WrongPersonalInfoScreen.route,
-//                                   arguments: {'isEmail': true},
-//                                 );
-//                               },
-//                               child: Text("Não é seu email? Clique aqui",
-//                                   style: TextStyle(
-//                                     fontWeight: FontWeight.w500,
-//                                     fontSize: 18,
-//                                     fontFamily: 'Roboto',
-//                                     color: Color(0xFF136048),
-//                                     decoration: TextDecoration.underline,
-//                                   ))))
-//                     ],
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-//
-//   Widget _buildNumberText(String text) {
-//     return Text(
-//       text,
-//       style: GoogleFonts.roboto(
-//         fontWeight: FontWeight.w400,
-//         color: Color(0xFF515151),
-//         fontSize: 40,
-//       ),
-//     );
-//   }
-//
-//   Widget _buildInputField(TextEditingController controller) {
-//     return Container(
-//       width: 26,
-//       height: 40,
-//       child: TextField(
-//         controller: controller,
-//         textAlign: TextAlign.center,
-//         keyboardType: TextInputType.number,
-//         maxLength: 1,
-//         style: GoogleFonts.roboto(
-//           fontWeight: FontWeight.w400,
-//           color: Color(0xFF515151),
-//           fontSize: 16,
-//         ),
-//         decoration: InputDecoration(
-//           filled: true,
-//           fillColor: Color(0xFFF2F2F2),
-//           counterText: "",
-//           border: OutlineInputBorder(
-//             borderSide: BorderSide.none,
-//             borderRadius: BorderRadius.circular(8),
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-//
-//   Widget _buildRadioOption(String email) {
-//     if (email.isEmpty) {
-//       return SizedBox.shrink(); // If email is empty, return an empty SizedBox
-//     }
-//
-//     // Split the email into parts before and after '@'
-//     List<String> emailParts = email.split('@');
-//     if (emailParts.length != 2) {
-//       return SizedBox
-//           .shrink(); // If email format is invalid, return an empty SizedBox
-//     }
-//
-//     String maskedEmail =
-//         '${emailParts[0].substring(0, 2)}${'*' * (emailParts[0].length - 2)}@${emailParts[1]}';
-//
-//     return InkWell(
-//       onTap: () {
-//         setState(() {
-//           useOtherEmail = true; // Always toggle to 'Use another email' option
-//         });
-//       },
-//       child: Row(
-//         children: [
-//           Radio(
-//             value: maskedEmail,
-//             groupValue: useOtherEmail ? 'Usar outro email' : maskedEmail,
-//             onChanged: (value) {
-//               setState(() {
-//                 useOtherEmail = value == 'Usar outro email';
-//               });
-//             },
-//             activeColor: Colors.green, // Selected color
-//           ),
-//           Text(
-//             maskedEmail,
-//             style: GoogleFonts.roboto(
-//               fontWeight: FontWeight.w500,
-//               color: Color(0xFF515151),
-//               fontSize: 16,
-//             ),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
-//
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -258,12 +5,13 @@ import 'package:http/http.dart' as http;
 import 'package:passaqui/src/modules/hire/biometria/hire_biometria_screen.dart';
 import 'package:passaqui/src/shared/widget/appbar.dart';
 import 'package:passaqui/src/shared/widget/button.dart';
-import 'package:url_launcher/url_launcher_string.dart';
 
 import '../../../core/di/service_locator.dart';
 import '../../../core/navigation/navigation_handler.dart';
 import '../../../services/auth_service.dart';
 import '../wrongInfo/wrong_cpf_screen.dart';
+
+final RouteObserver<PageRoute> routeObserver = RouteObserver<PageRoute>();
 
 class HireConfirmEmailScreen extends StatefulWidget {
   static const String route = "/hire-confirm-email";
@@ -275,7 +23,8 @@ class HireConfirmEmailScreen extends StatefulWidget {
   State<HireConfirmEmailScreen> createState() => _HireConfirmEmailScreenState();
 }
 
-class _HireConfirmEmailScreenState extends State<HireConfirmEmailScreen> {
+class _HireConfirmEmailScreenState extends State<HireConfirmEmailScreen>
+    with RouteAware {
   bool useOtherEmail = false; // Track the state of the radio buttons
   final AuthService _authService = AuthService();
   bool _isLoading = false;
@@ -286,6 +35,31 @@ class _HireConfirmEmailScreenState extends State<HireConfirmEmailScreen> {
   void initState() {
     super.initState();
     _fetchUserEmail(); // Fetch user's email when screen initializes
+    print("Quack quack iniciado");
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    final route = ModalRoute.of(context);
+    if (route is PageRoute) {
+      routeObserver.subscribe(this, route);
+      print("Subscribed to RouteObserver");
+    }
+    //chamada aqui
+  }
+
+  @override
+  void dispose() {
+    routeObserver.unsubscribe(this);
+    print("Unsubscribed from RouteObserver");
+    super.dispose();
+  }
+
+  @override
+  void didPopNext() {
+    super.didPopNext();
+    _fetchUserEmail(); // Fetch email again or call any other function
   }
 
   Future<void> _fetchUserEmail() async {
@@ -508,40 +282,3 @@ class _HireConfirmEmailScreenState extends State<HireConfirmEmailScreen> {
     );
   }
 }
-
-// class BiometriaScreen extends StatefulWidget {
-//   static const String route = "/hire-biometria";
-//   final String cpf;
-//
-//   const BiometriaScreen({required this.cpf, Key? key}) : super(key: key);
-//
-//   @override
-//   State<BiometriaScreen> createState() => _BiometriaScreenState();
-// }
-//
-// class _BiometriaScreenState extends State<BiometriaScreen> {
-//   late final WebViewController controller;
-//   final String baseUrl =
-//       'http://passcash-api-hml.us-east-1.elasticbeanstalk.com';
-//
-//   @override
-//   void initState() {
-//     super.initState();
-//     controller = WebViewController()
-//       ..loadRequest(
-//         Uri.parse('$baseUrl/api/ApiMaster/iniciarBiometria?cpf=${widget.cpf}')
-//       );
-//   }
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: const Text('Flutter WebView'),
-//       ),
-//       body: WebViewWidget(
-//         controller: controller,
-//       ),
-//     );
-//   }
-// }
--- lib/src/shared/widget/appbar.dart
+++ lib/src/shared/widget/appbar.dart
@@ -77,6 +77,60 @@
 // }
 
 
+// import 'package:flutter/material.dart';
+// import 'package:flutter_svg/flutter_svg.dart';
+//
+// class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
+//   final bool showBackButton;
+//   final bool showLogo;
+//   final String? title;
+//
+//   const PassaquiAppBar({
+//     this.showBackButton = false,
+//     this.showLogo = true,
+//     this.title,
+//     Key? key,
+//   }) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return AppBar(
+//       key: key,
+//       elevation: 0,
+//       backgroundColor: Theme.of(context).primaryColor,
+//       automaticallyImplyLeading: showBackButton,
+//       title: Row(
+//         children: [
+//           if (showLogo)
+//             Padding(
+//               padding: const EdgeInsets.only(left: 1.0),
+//               child: Image.asset(
+//                 "assets/images/logo_passaqui_hor_1.png",
+//                 fit: BoxFit.contain,
+//                 height: 100,
+//                 width: 90,
+//               ),
+//             ),
+//           if (title != null) // If title is provided, show it
+//             Expanded(
+//               child: Text(
+//                 title!,
+//                 style: TextStyle(
+//                   fontSize: 20,
+//                   fontWeight: FontWeight.w500,
+//                 ),
+//                 textAlign: TextAlign.start,
+//               ),
+//             ),
+//         ],
+//       ),
+//     );
+//   }
+//
+//   @override
+//   Size get preferredSize => const Size.fromHeight(56.0);
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -84,11 +138,13 @@ class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
   final bool showBackButton;
   final bool showLogo;
   final String? title;
+  final VoidCallback? onBackButtonPressed; // Add this line
 
   const PassaquiAppBar({
     this.showBackButton = false,
     this.showLogo = true,
     this.title,
+    this.onBackButtonPressed, // Add this line
     Key? key,
   }) : super(key: key);
 
@@ -98,7 +154,15 @@ class PassaquiAppBar extends StatelessWidget implements PreferredSizeWidget {
       key: key,
       elevation: 0,
       backgroundColor: Theme.of(context).primaryColor,
-      automaticallyImplyLeading: showBackButton,
+      automaticallyImplyLeading: false, // Set this to false to use custom leading
+      leading: showBackButton
+          ? IconButton(
+        icon: Icon(Icons.arrow_back),
+        onPressed: onBackButtonPressed ?? () { // Add this line
+          Navigator.of(context).pop(); // Default action if callback is not provided
+        },
+      )
+          : null,
       title: Row(
         children: [
           if (showLogo)
